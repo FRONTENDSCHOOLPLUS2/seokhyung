6f3ecee7a0ff6fc7f53c2bd33b6923122f62097c
seok-hyung <korea970223@naver.com>
2024-07-16 20:46:09 +0900

    routes 수정

--- ts/src/routes.tsx
+++ ts/src/routes.tsx
@@ -1,50 +1,50 @@
-import Layout from "@components/layout";
-import Error from "@pages/Error";
-import Community from "@pages/community";
-import Detail from "@pages/community/Detail";
-import List from "@pages/community/List";
-import New from "@pages/community/New";
-import Login from "@pages/user/Login";
-import Signup from "@pages/user/Signup";
-import { createBrowserRouter } from "react-router-dom";
-import Edit from "@pages/community/Edit";
+import Layout from '@components/layout';
+import Error from '@pages/Error';
+import Community from '@pages/community';
+import Detail from '@pages/community/Detail';
+import List from '@pages/community/List';
+import New from '@pages/community/New';
+import Login from '@pages/user/Login';
+import Signup from '@pages/user/Signup';
+import { createBrowserRouter } from 'react-router-dom';
+import Edit from '@pages/community/Edit';
 
 // React Router v6.4(2022.08.23)
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     errorElement: <Error />,
     element: <Layout />,
     children: [
       {
         index: true,
-        element: <Community />
+        element: <Community />,
       },
       {
-        path: ":type",
-        element: <List />
+        path: ':type',
+        element: <List />,
       },
       {
-        path: ":type/:_id",
-        element: <Detail />
+        path: ':type/:_id',
+        element: <Detail />,
       },
       {
-        path: ":type/new",
-        element: <New />
+        path: ':type/new',
+        element: <New />,
       },
       {
-        path: ":type/:_id/edit",
-        element: <Edit />
+        path: ':type/:_id/edit',
+        element: <Edit />,
       },
       {
-        path: "user/login",
-        element: <Login />
+        path: 'user/login',
+        element: <Login />,
       },
       {
-        path: "user/signup",
-        element: <Signup />
+        path: 'user/signup',
+        element: <Signup />,
       },
-    ]
+    ],
   },
 ]);
 
